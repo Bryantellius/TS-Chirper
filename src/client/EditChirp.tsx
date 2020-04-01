1e4ca955d9ef5d1fa74a58329cd20a09684d5dab
Ben Bryant <brbryant2639@gmail.com>
2020-04-01 18:26:38 -0500

    added history.back() to update button

--- src/client/EditChirp.tsx
+++ src/client/EditChirp.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
+import * as ReactComponentProps from "react-router-dom";
 
-const EditChirp = () => {
+const EditChirp: React.SFC<IEditProps> = props => {
+  const saveEdit = () => {
+    history.back();
+  };
+
   return (
     <div>
       <h1>Edit Chirp</h1>
@@ -10,9 +15,13 @@ const EditChirp = () => {
       <div className="form-group">
         <textarea className="form-control" id="chirpBody"></textarea>
       </div>
-      <button className="btn btn-success">Update</button>
+      <button className="btn btn-success" onClick={saveEdit}>
+        Update
+      </button>
     </div>
   );
 };
+
+interface IEditProps {}
 
 export default EditChirp;
