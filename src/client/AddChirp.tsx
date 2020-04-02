adbd1554b097b9a0802e7b074c9cd8aa40169ebd
Ben Bryant <brbryant2639@gmail.com>
2020-04-02 11:25:52 -0500

    added form to add chirp info and posts to chirp.json

--- src/client/AddChirp.tsx
+++ src/client/AddChirp.tsx
@@ -1,7 +1,47 @@
 import * as React from "react";
+import $ from "jquery";
 
 const AddChirp: React.SFC = props => {
-  return <h1>Add Chirp Here</h1>;
+  const post = () => {
+    console.log(`addBtn pressed`);
+    let chirp = {
+      username: $("#chirpAuthor").val(),
+      message: $("#chirpBody").val()
+    };
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/api/chirps",
+      contentType: "application/json",
+      data: JSON.stringify(chirp)
+    }).done(res => {
+      console.log(res);
+    });
+    history.back();
+  };
+
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <h1>Add Chirp</h1>
+      <div className="form-group w-50">
+        <input
+          type="text"
+          className="form-control"
+          id="chirpAuthor"
+          placeholder="Name"
+        ></input>
+      </div>
+      <div className="form-group w-50">
+        <textarea
+          className="form-control"
+          id="chirpBody"
+          placeholder="Enter Text"
+        ></textarea>
+      </div>
+      <button onClick={post} className="btn btn-success">
+        Post
+      </button>
+    </div>
+  );
 };
 
 export default AddChirp;
