c55c6b9409fe5202da71063fef93b747c8d7a500
Ben Bryant <brbryant2639@gmail.com>
2020-04-02 11:26:52 -0500

    added update function for put requests, delete button for corresponding requests

--- src/client/EditChirp.tsx
+++ src/client/EditChirp.tsx
@@ -1,8 +1,28 @@
 import * as React from "react";
-import * as ReactComponentProps from "react-router-dom";
+import $ from "jquery";
 
 const EditChirp: React.SFC<IEditProps> = props => {
   const saveEdit = () => {
+    let editedChirp = {
+      username: $("#editAuthor").val(),
+      message: $("#editBody").val()
+    };
+    $.ajax({
+      method: "PUT",
+      url: `http://localhost:3000/api/chirps/5`,
+      contentType: "application/json",
+      data: JSON.stringify(editedChirp)
+    }).done(res => {
+      console.log(res);
+    });
+    history.back();
+  };
+
+  const deleteChirp = () => {
+    $.ajax({
+      method: "DELETE",
+      url: `http://localhost:3000/api/chirps/5`
+    }).done(res => console.log(res));
     history.back();
   };
 
@@ -10,14 +30,19 @@ const EditChirp: React.SFC<IEditProps> = props => {
     <div>
       <h1>Edit Chirp</h1>
       <div className="form-group">
-        <input type="text" className="form-control" id="chirpAuthor"></input>
+        <input type="text" className="form-control" id="editAuthor"></input>
       </div>
       <div className="form-group">
-        <textarea className="form-control" id="chirpBody"></textarea>
+        <textarea className="form-control" id="editBody"></textarea>
       </div>
-      <button className="btn btn-success" onClick={saveEdit}>
-        Update
-      </button>
+      <div className="editBtns d-flex justify-content-between">
+        <button className="btn btn-success" onClick={saveEdit}>
+          Update
+        </button>
+        <button className="btn btn-danger" onClick={deleteChirp}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
